473190595e49990ea624fb3baefab30aed479603
datta walse <dattawalse97@gmail.com>
2026-04-17 00:47:38 +0530

    here we are login to Orange hrm application and verify the login is successful or not by using cypress framework and we are using cypress for end to end testing and we are using cypress for automation testing and we are using cypress for testing the login functionality of the Orange hrm application and we are

---
+++ tests/Login.spec.js
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test';
+//Navigate to orangehrm login page
+
+
+//Enter username and password and click on login button
+test('Enter username and password and click on login button', async ({page}) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('Admin');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('admin123');
+  await page.screenshot({ path: 'login-success.png' });
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+  await page.screenshot({ path: 'login-success.png' });
+});
