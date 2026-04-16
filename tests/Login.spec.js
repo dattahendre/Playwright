b0f54678b5d20acf7efaf901cc8353c28c9f2179
datta walse <dattawalse97@gmail.com>
2026-04-17 00:57:08 +0530

    create folder for screenshot and move login-success.png to screenshot folder, update Login.spec.js to reflect the change

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -9,8 +9,9 @@ test('Enter username and password and click on login button', async ({page}) => 
   await page.getByPlaceholder('Username').fill('Admin');
   await page.getByPlaceholder('Password').click();
   await page.getByPlaceholder('Password').fill('admin123');
-  await page.screenshot({ path: 'login-success.png' });
+  await page.screenshot({ path: 'D:\\Swamini\\Playwright\\screenshot\\login-success.png' });
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
-  await page.screenshot({ path: 'login-success.png' });
+  await page.screenshot({ path: 'D:\\Swamini\\Playwright\\screenshot\\login-dashboard.png' });
+  await page.pause();
 });
